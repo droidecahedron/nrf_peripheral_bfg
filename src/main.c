a1584022960e906664dea102377f6b36d8caedff
nrf_ddhd <droidecahedron@gmail.com>
2026-03-04 16:34:59 -0800

    impl

--- src/main.c
+++ src/main.c
@@ -4,10 +4,33 @@
  * Bluetooth fuel gauge seeed board workshop main file
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
- 
+
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+static const struct gpio_dt_spec bt_status_led = GPIO_DT_SPEC_GET(DT_NODELABEL(led0), gpios);
 
 int main(void)
 {
+    int err;
+
+    err = gpio_pin_configure_dt(&bt_status_led, GPIO_OUTPUT_INACTIVE);
+    if (err)
+    {
+        LOG_WRN("Configuring BT status LED failed (err %d)", err);
+    }
+
+    for (;;)
+    {
+        gpio_pin_set_dt(&bt_status_led, 1);
+        k_msleep(500);
+        LOG_INF("I am alive");
+        gpio_pin_set_dt(&bt_status_led, 0);
+        k_msleep(2000);
+    }
     return 0;
 }
